a230f411603e78b98d7a7dc0b30edb78a19878da
ShibanandaJena <jenashivananda@gmail.com>
2024-09-19 18:46:49 +0530

    minor update

--- ReactBlogApp/src/App.jsx
+++ ReactBlogApp/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL)
   const [count, setCount] = useState(0)
 
   return (
-    <>
- 
- <div>
-  <h1>This is a blog app</h1>
- </div>
+    <> 
+      <div>
+        <h1>This is a blog app</h1>
+      </div>
     </>
   )
 }
