9767b70381cbd63ad8a956d9609fa43df7878412
ShibanandaJena <jenashivananda@gmail.com>
2024-09-24 17:52:40 +0530

    Postform setup

--- ReactBlogApp/src/components/index.js
+++ ReactBlogApp/src/components/index.js
@@ -4,6 +4,20 @@ import Container from './Container';
 import Logo from './Logo';
 import LogoutBtn from './LogoutBtn';
 import Button from './Button';
+import RTE from './RTE';
+import Input from './Input'
+import Select from './Select';
 
 
-export { Header, Footer, Container, Logo, LogoutBtn,Button };
+
+export { 
+    Header, 
+    Footer, 
+    Container, 
+    Logo, 
+    LogoutBtn,
+    Button,
+    RTE ,
+    Input,
+    Select
+    };
